9bf13e0c653f945126316399831b37a9a8bcfe99
Charles Robison <crobison@uw.edu>
2016-04-06 16:50:51 -0700

    Added a comment

--- private_critter.py
+++ private_critter.py
@@ -12,4 +12,16 @@ class Critter(object):
         print("\nI'm", self.name)
         print("Right now I feel", self.__mood, "\n")
 
-# some comment
+    def __private_method(self):
+        print("This is a private method.")
+
+    def public_method(self):
+        print("This is a public method.")
+        self.__private_method()
+
+# main
+crit = Critter(name = "Poochie", mood = "happy")
+crit.talk()
+crit.public_method()
+
+input("\n\nPress the enter key to exit.")
